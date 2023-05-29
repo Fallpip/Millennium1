0acfedd1afd0cce194ec72eeddd5faf359bbbdf0
Fallpip <121276927+Fallpip@users.noreply.github.com>
2023-05-29 10:31:31 +0300

    Update grrr.sql

--- grrr.sql
+++ grrr.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: gr
 -- ------------------------------------------------------
--- Server version	8.0.26
+-- Server version	8.0.26sss
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
